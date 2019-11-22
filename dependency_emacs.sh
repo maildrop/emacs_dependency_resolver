ae360977a0fe2e597c4c698fba1307d1bc890b06
TOGURO Mikito <mit@shalab.net>
2019-11-22 10:46:57 +0900

    M-x package-list-package で署名の検証に失敗していた理由の一つとして、gnutls が libgnutsxx-28.dll も要求していたので、これを追加

--- dependency_emacs.sh
+++ dependency_emacs.sh
@@ -2,7 +2,7 @@
 
 #emacs が暗黙的 あるいは明示的にリンクしている DLL これは、バージョンアップに伴って変わる可能性がある。
 #dependency_dlls="libjpeg-8.dll libXpm-noX4.dll libgif-7.dll libgdk-win32-2.0-0.dll libpng16-16.dll libtiff-5.dll libxml2-2.dll libdbus-1-3.dll librsvg-2-2.dll libgnutls-28.dll"
-dependency_dlls="libjpeg-8.dll libXpm-noX4.dll libgif-7.dll libpng16-16.dll libtiff-5.dll libxml2-2.dll librsvg-2-2.dll  libwinpthread-1.dll libgnutls-30.dll"
+dependency_dlls="libjpeg-8.dll libXpm-noX4.dll libgif-7.dll libpng16-16.dll libtiff-5.dll libxml2-2.dll librsvg-2-2.dll libwinpthread-1.dll libgnutls-30.dll libgnutlsxx-28.dll"
 w32_imeadv_dlls= #"libgcc_s_seh-1.dll libstdc++-6.dll"
 # 依存ファイルのリストを作成する。
 if [ -d .deplist ] ; then  # 存在していたら作り直し
